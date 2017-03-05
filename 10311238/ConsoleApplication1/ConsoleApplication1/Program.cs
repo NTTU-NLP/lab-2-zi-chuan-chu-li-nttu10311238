807d76a61d4527247567d6292eb285159767272d
nttu10311238 <byc2096@gmail.com>
2017-03-05 23:26:39 +0800

    LAB2

---
+++ 10311238/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            using (StreamReader sr = new StreamReader(@"..\..\..\..\..\Data\Input.txt"))
+            {
+                while (sr.Peek() != -1)
+                {
+                    string line = sr.ReadLine();
+                    foreach (var ch in line)
+                    {
+                        if (!char.IsWhiteSpace(ch))
+                        {
+                            if (ch == 46)
+                            {
+                                Console.Write(".\n");
+                            }
+                            else if (ch == 63)
+                            {
+                                Console.Write("?\n");
+                            }
+                            else if (ch == 33)
+                            {
+                                Console.Write("!\n");
+                            }
+                            else
+                                Console.Write(ch);
+
+                        }
+
+                    }
+
+                }
+            }
+            Console.Read();
+        }
+    }
+}
